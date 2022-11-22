aa20f800e4f5b6a1ed4b35f390a1f4a621faadce
Dnyaneshwar Sawant <dnyanasawant111@gmail.com>
2022-11-22 14:23:13 +0530

    contact inserted in file

---
+++ src/Components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>Home
+        <h1>HII</h1>
+     <img src="https://images.unsplash.com/photo-1541963463532-d68292c34b19?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mnx8fGVufDB8fHx8&w=1000&q=80"></img>
+    </div>
+  )
+}
+
+export default Home
